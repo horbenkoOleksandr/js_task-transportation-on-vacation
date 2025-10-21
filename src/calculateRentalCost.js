8dde387e25ebe00fa0b231e9fcd91ba8c5695c0d
Oleksandr Horbenko <oleksandr.horbenko.01@gmail.com>
2025-10-21 13:04:12 +0300

    New file

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const price = 40;
+  const sum = price * days;
+
+  if (days >= 7) {
+    return sum - 50;
+  }
+
+  if (days >= 3) {
+    return sum - 20;
+  }
+
+  return sum;
 }
+
+calculateRentalCost(1);
+calculateRentalCost(3);
+calculateRentalCost(7);
 
 module.exports = calculateRentalCost;
